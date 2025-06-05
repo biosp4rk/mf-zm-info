bcb8f0fb117f610e880a4555ae0633271f72eed6
biosp4rk <37962487+biosp4rk@users.noreply.github.com>
2025-06-05 14:27:14 -0700

    Print sorted elf entries

--- tools/decomp/elf_parser.py
+++ tools/decomp/elf_parser.py
@@ -87,58 +87,58 @@ def parse_elf_line(line: str) -> ElfSym:
     return ElfSym(value, size, kind, bind, ndx, name)
 
 
-def get_entry_names(entries: list[ElfSym]) -> dict[int, str]:
+def print_entries(entries: list[ElfSym], exclude_ram: bool = False) -> None:
+    # Remove entries that don't represent RAM, code, or data
     entries = [
         e for e in entries
-        if e.ndx >= 1 and (
-            e.bind == ElfBind.GLOBAL or (e.kind != ElfType.SECTION and e.ndx == 3)
-        )
+        if e.kind != ElfType.FILE and e.kind != ElfType.SECTION and e.ndx >= 1
     ]
-    entries.sort(key=lambda e: e.value)
-    entry_names = {}
+    # Sort by address
+    entries.sort(key=lambda x: x.value)
+    # Remove RAM entries
+    if exclude_ram:
+        entries = [
+            e for e in entries
+            if e.value >= ROM_OFFSET
+        ]
+    # Fix THUMB function addresses
     for entry in entries:
-        addr = entry.value
-        assert addr >= 0x2000000
-        if addr >= ROM_OFFSET:
-            addr -= ROM_OFFSET
-        if entry.kind == ElfType.FUNC:
-            if addr % 2 == 1:
-                addr -= 1
-        entry_names[addr] = entry.name
-    return entry_names
+        if entry.kind == ElfType.FUNC and entry.value % 2 != 0:
+            entry.value -= 1
+    for entry in entries:
+        print(f"{entry.value:X}\t{entry.size:X}\t{entry.name}")
 
 
-def update_info_files(game: str, region: str, entry_names: dict[int, str]) -> None:
-    # Find all yaml files
-    yaml_files = []
-    for map_type in (MAP_CODE, MAP_RAM, MAP_DATA):
-        paths = ifu.find_yaml_files(game, map_type, True)
-        yaml_files += [(p, map_type) for p in paths]
-    # Parse files and output
-    for path, map_type in yaml_files:
-        name = os.path.basename(path)
-        print(f"Checking {game} {map_type} {name}")
-        data = ifu.load_yaml_file(path)
-        ifile = ifu.parse_obj_list(data, map_type)
-        for entry in ifile:
-            addr = entry.addr
-            if isinstance(addr, dict):
-                addr = addr.get(region)
-                if addr is None:
-                    continue
-            name = entry_names.get(addr)
-            if name is not None:
-                entry.name = name
-        ifu.write_info_file(path, map_type, ifile)
+# def update_info_files(game: str, region: str, entry_names: dict[int, str]) -> None:
+#     # Find all yaml files
+#     yaml_files = []
+#     for map_type in (MAP_CODE, MAP_RAM, MAP_DATA):
+#         paths = ifu.find_yaml_files(game, map_type, True)
+#         yaml_files += [(p, map_type) for p in paths]
+#     # Parse files and output
+#     for path, map_type in yaml_files:
+#         name = os.path.basename(path)
+#         print(f"Checking {game} {map_type} {name}")
+#         data = ifu.load_yaml_file(path)
+#         ifile = ifu.parse_obj_list(data, map_type)
+#         for entry in ifile:
+#             addr = entry.addr
+#             if isinstance(addr, dict):
+#                 addr = addr.get(region)
+#                 if addr is None:
+#                     continue
+#             name = entry_names.get(addr)
+#             if name is not None:
+#                 entry.name = name
+#         ifu.write_info_file(path, map_type, ifile)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("map_path", type=str, help="Path to linker map file")
-    parser.add_argument("game", type=str, choices=GAMES, help="Game abbreviation")
-    parser.add_argument("region", type=str, choices=REGIONS, help="Region abbreviation")
+    parser.add_argument("elf_path", type=str, help="Path to elf file")
+    # parser.add_argument("game", type=str, choices=GAMES, help="Game abbreviation")
+    # parser.add_argument("region", type=str, choices=REGIONS, help="Region abbreviation")
     
     args = parser.parse_args()
-    entries = parse_elf_file(args.map_path)
-    entry_names = get_entry_names(entries)
-    update_info_files(args.game, args.region, entry_names)
+    entries = parse_elf_file(args.elf_path)
+    print_entries(entries, True)
